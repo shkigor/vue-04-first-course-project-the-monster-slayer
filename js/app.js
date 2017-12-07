68911ee9efd7b0b63a665beb4a18917ff43840d9
Igor Shklyar <igor.shklyar@gmail.com>
2017-12-07 16:51:38 +0200

    050 Implementing a Start Game Method Also added plug implementing a Give up Method

--- js/app.js
+++ js/app.js
@@ -4,5 +4,15 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning : false
+    },
+    methods: {
+        startGame: function () {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        giveUpGame: function () {
+            this.gameIsRunning = false;
+        }
     }
 });
