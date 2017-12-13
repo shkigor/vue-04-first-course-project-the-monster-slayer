1d3eacec2abb75be30718ec9acec1fd93f8705ef
Igor Shklyar <igor.shklyar@gmail.com>
2017-12-13 14:22:24 +0200

    051 Implementing a Attack Method

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning : false
+        gameIsRunning: false
     },
     methods: {
         startGame: function () {
@@ -11,8 +11,37 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        giveUpGame: function () {
+        giveUp: function () {
             this.gameIsRunning = false;
+        },
+        attack: function () {
+            var max = 10;
+            var min = 3;
+            var damage = Math.min(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 3;
+            damage = Math.min(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRunning = false;
+            }
+
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
         }
     }
 });
