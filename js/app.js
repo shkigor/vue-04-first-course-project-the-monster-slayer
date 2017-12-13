79aaaf411a39329db2a5d1bcb0f095a1f1ace141
Igor Shklyar <igor.shklyar@gmail.com>
2017-12-13 16:00:21 +0200

    056 Creating an Action Log

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
@@ -15,7 +16,12 @@ new Vue({
             this.gameIsRunning = false;
         },
         attack: function () {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -37,8 +43,13 @@ new Vue({
             this.monsterAttacks();
         },
         monsterAttacks: function () {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
         calculateDamage: function (min, max) {
             return Math.min(Math.floor(Math.random() * max) + 1, min);
